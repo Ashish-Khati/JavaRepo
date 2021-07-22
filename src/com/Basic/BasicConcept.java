66c2a798e3bae3839e25611d9a1d6d15ad59fa0c
Ashish_Khati <khatiashish2442@gmail.com>
2021-07-22 20:11:03 +0530

    Initial commit

--- src/com/Basic/BasicConcept.java
+++ src/com/Basic/BasicConcept.java
@@ -5,8 +5,7 @@ public class BasicConcept {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! Welcome to Java basic programming. Here we enhance our programming skills by covering core java programming,because basic concepts play importent role to be a good programmer.So lets start together!! ");
-		
-
+		//The quick brown fox jumps over the lazy dog. 
 	}
 
 }
